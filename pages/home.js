f9c5844b31fcb61ade635a8c725928df216973b1
Sheeeennn <127721330+Sheeeenn@users.noreply.github.com>
2025-05-16 22:54:30 +0800

    [Added] Firebase and home

---
+++ pages/home.js
@@ -0,0 +1,19 @@
+// pages/Home.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
